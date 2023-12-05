76419a8663b12ce3f0988832ee7e897429f76f97
BiljanaDa <bibidakovic@gmail.com>
2023-12-05 20:21:20 +0100

    init

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.default')
+
+
+@section('content')
+
+<h1>Welcome to my blog</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero quibusdam obcaecati minus, quam iure placeat iusto dolorem blanditiis ducimus temporibus vitae, dolores, doloremque eveniet illo optio modi quae at necessitatibus reiciendis repellat odio suscipit ipsum nisi. Obcaecati aspernatur consequatur nemo laboriosam? Suscipit ab itaque officiis in. Quae quasi officiis dicta, officia iste consequuntur error laboriosam sunt veritatis architecto ipsa repellat modi repellendus, nemo quos quisquam adipisci? Quaerat error hic id perferendis officiis, praesentium doloribus culpa! Illo adipisci veritatis nesciunt delectus ullam. Soluta, molestiae tempora enim distinctio delectus vitae deleniti corporis, hic nam error tempore id nostrum debitis aut ratione magnam ipsam quam alias deserunt magni! Distinctio necessitatibus sapiente fugit aperiam quas voluptatem officiis aliquid velit debitis. Eveniet doloremque, a inventore facere distinctio voluptatum veritatis! Deserunt maiores libero facilis totam dolore sapiente saepe ab. Maxime quasi qui, modi cumque aut exercitationem deserunt dolor in, voluptate aliquid, perspiciatis sapiente! Eos recusandae, aspernatur officia deleniti commodi voluptatem cumque delectus quas quod quos sit rerum fuga debitis, provident quibusdam? Tempore saepe rerum laudantium aut veniam ut aliquam blanditiis distinctio facilis, officia deserunt iure unde vitae adipisci amet atque dolores corporis necessitatibus similique, optio a debitis. Voluptate dolorum maiores perspiciatis porro hic et. Nemo, itaque.</p>
+
+@endsection
